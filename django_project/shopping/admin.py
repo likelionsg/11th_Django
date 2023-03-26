b62ff35c4eb23256d9f2b4f4ec6a57adb51e407d
jongmee <ra123221@gmail.com>
2023-03-26 18:36:22 +0900

    shopping model을 admin 페이지에 등록하기

--- django_project/shopping/admin.py
+++ django_project/shopping/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Shopping
 
-# Register your models here.
+class ShoppingAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Shopping, ShoppingAdmin)
