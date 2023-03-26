821cd388edc49c3a6680e6fff8345a3c60876fae
jongmee <ra123221@gmail.com>
2023-03-26 21:37:35 +0900

    아이템 정보와 함께 상세 페이지를 렌더링하는 메서드 작성하기

--- django_project/shopping/views.py
+++ django_project/shopping/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Shopping
 
 def shopping_list(request):
     qs = Shopping.objects.all()
     return render(request, 'index.html', {'item_list': qs,})
+
+def detail(request, pk):
+    item = get_object_or_404(Shopping, pk=pk)
+    return render(request, 'detail.html', {'item':item,})
