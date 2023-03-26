3ba0e73ec3f4062f3c5eac0f0ca293aab6a1d6cb
jongmee <ra123221@gmail.com>
2023-03-26 20:35:33 +0900

    store model 생성하기

--- django_project/store/models.py
+++ django_project/store/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(max_length=50)
