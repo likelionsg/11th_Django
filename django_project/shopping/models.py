6b5bb590016151469d72968e6a09c2f7b41a3273
jongmee <ra123221@gmail.com>
2023-03-26 20:57:18 +0900

    shopping model에서 store model을 foreign key로 가져오기

--- django_project/shopping/models.py
+++ django_project/shopping/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from store.models import Store
 
 class Shopping(models.Model): # django의 Model class를 상속받음
     def __str__(self):
         return self.name
     
     name = models.CharField(max_length=50)
-    store = models.CharField(max_length=100)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
     count = models.IntegerField()
     price = models.IntegerField()
 
