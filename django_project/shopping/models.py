3109ee68877bdea0654a4a2a4f2182adf9566861
jongmee <ra123221@gmail.com>
2023-03-26 18:19:04 +0900

    shopping model 생성하기

--- django_project/shopping/models.py
+++ django_project/shopping/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Shopping(models.Model): # django의 Model class를 상속받음
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(max_length=50)
+    store = models.CharField(max_length=100)
+    count = models.IntegerField()
+    price = models.IntegerField()
+
