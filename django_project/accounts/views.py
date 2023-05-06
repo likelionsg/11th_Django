3355d3f21803bcfcb27c5bfdbd53ee3de140d95d
jongmee <ra123221@gmail.com>
2023-05-07 00:42:27 +0900

    로그인 기능 구현

--- django_project/accounts/views.py
+++ django_project/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
-# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        userid = request.POST['username']
+        pwd = request.POST['password']
+        user = auth.authenticate(request, username=userid, password=pwd)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('list_page')
+        else:
+            return render(request, 'login_form.html')
+    else:
+        return render(request, 'login_form.html')
