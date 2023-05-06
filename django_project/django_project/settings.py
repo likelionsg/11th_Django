3a61c90ee87f121f7757d9dadd82f4d9f56c56f3
jongmee <ra123221@gmail.com>
2023-05-06 23:49:14 +0900

    커스텀 유저 모델을 유저 모델로 설정하기

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -134,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'		
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# custom user model
+
+AUTH_USER_MODEL = 'accounts.User'
